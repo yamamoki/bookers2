5c51cc6fd62964db6e4f7bd3175fbb737def574a
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-10-15 04:19:16 +0000

    [Add]bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users #deviseを上にしておく
-  resources :books,only: [:edit, :index, :show]
-  resources :users, only: [:edit, :index, :show]
   root to: 'homes#top'
+  resources :books, only: [:edit, :index, :show]
+  resources :users, only: [:edit, :index, :show]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
