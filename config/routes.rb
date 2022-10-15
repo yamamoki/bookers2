b75941fb2ee7775b44b43e690f8054a6c1b78fd1
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-10-15 08:40:43 +0000

    [Add]bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   resources :books, only: [:edit, :index, :show]
   resources :users, only: [:edit, :index, :show]
-  
+  get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
