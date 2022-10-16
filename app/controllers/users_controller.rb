76fa1c652a33a5fd5950c4a03aa6cd1f2915e06b
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-10-16 02:50:52 +0000

    [Add]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,15 @@ class UsersController < ApplicationController
     #@books = @user.books
   end
 
+   def create
+    #@user = User.new(params[:user])   #これはRails3までの実装
+    @user = User.new(user_params)
+    if @user.save
+      #保存の成功をここで扱う
+      redirect_to @user
+    else
+      render 'new'
+    end
+   end
+
 end
