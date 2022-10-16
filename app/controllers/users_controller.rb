e77610521b584083e3884f81ff128b50407c24f0
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-10-16 12:53:11 +0000

    [Add]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  
+
   def edit
+    @user=User.new
   end
 
   def index
@@ -13,12 +14,12 @@ class UsersController < ApplicationController
 
    def create
     #@user = User.new(params[:user])   #これはRails3までの実装
-    @user = User.new(user_params)
+    @user = User.find(params[:id])
     if @user.save
       #保存の成功をここで扱う
-      redirect_to @user
+      redirect_to '/users/:id'
     else
-      render 'new'
+      render 'root'
     end
    end
 
