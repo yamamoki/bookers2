54209e94b4cd336c0b4833b3c0e1e246b5c27dbc
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-10-16 17:51:18 +0000

    [Add]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,40 +1,18 @@
 class BooksController < ApplicationController
   def edit
+     @books =Book.new
   end
 
   def index
-    @books=Book.all#これで投稿したのが出る
-    @book=Book.new
+    @books = Book.all
   end
 
-  def create#保存機能
-    @book=Book.new(book_params)
-    if @book.save
-    #投稿が成功
-    flash[:notice] ="Book was successfully created."
-    redirect_to book_path(@book)
-    else
-    @books=Book.all
-    render :index
-    end
-  end
-
-
-  def destroy
+def destroy
     @book = book.find(params[:id])
     @book.destroy
     redirect_to books_path
-  end
+end
 
   def show
   end
-
-private
-  # ストロングパラメータ
-  def book_params
-    params.require(:book).permit(:title, :body)
-  end
-  
-
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users #deviseを上にしておく
   root to: 'homes#top'
-  resources :users, only: [:show, :index, :edit, :update]
-  resources :books, only: [:edit, :index, :show, :destroy, :create]
+  resources :users, only: [:show, :index, :edit, :create, :update]
+  resources :books, only: [:edit, :index, :show, :destroy]
   
   get "/homes/about" => "homes#about", as: "about"
   
