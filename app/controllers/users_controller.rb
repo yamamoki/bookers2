f78dc98b02a0119a5ef13fdba9ca8d372aab82af
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-11-09 15:14:38 +0000

    gest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
  before_action :is_matching_login_user, only: [:edit, :update]
+ before_action :ensure_guest_user, only: [:edit]
 
   def edit
     @user = User.find(params[:id])
@@ -52,4 +53,10 @@ private
   end
   # ここまで追加
 
+   def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+   end
 end
