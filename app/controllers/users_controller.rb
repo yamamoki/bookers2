9122f67551c10a966b7a64948282acc54d3bceb6
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-12-27 06:54:53 +0000

    7b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,12 @@ class UsersController < ApplicationController
 
   def show
     @user=User.find(params[:id])#触らないたくさんのユーザーの一人
-    #@books = @user.books
     @users=User.all
     @books=@user.books
+    @today_book = @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
    def create
